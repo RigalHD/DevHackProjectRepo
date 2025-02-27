9b14f1c0aeebd5cb6939550d9ef3796e6f8ed43d
RigalHD <profoundromeo@gmail.com>
2025-02-27 15:13:32 +0300

    -

--- src/telegram_assistant/database/__init__.py
+++ src/telegram_assistant/database/__init__.py
@@ -2,4 +2,4 @@
 from telegram_assistant.database.db import Database, create_async_engine
 from telegram_assistant.database.models.base import BaseModel
 
-__all__ = ["BaseModel", "Database", "create_async_engine",]
+__all__ = ["BaseModel", "Database", "create_async_engine"]
