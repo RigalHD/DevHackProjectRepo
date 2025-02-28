f99061c92f397f7c30bdd917e3e46de78dac9782
RigalHD <profoundromeo@gmail.com>
2025-02-28 23:57:00 +0300

    refactored bad code

--- src/telegram_assistant/parser/url_parser.py
+++ src/telegram_assistant/parser/url_parser.py
@@ -1,26 +1,29 @@
-from telegram_assistant.parser.parser import Parser
-from bs4 import BeautifulSoup, Comment, Script
 import requests
+from bs4 import BeautifulSoup
 
-LINK_STAFF = "http://www.mmcs.sfedu.ru/faculty/staff"
+from telegram_assistant.parser.parser import Parser
+
+TEACHER_URL = "http://www.mmcs.sfedu.ru/faculty/staff"
+
 
 class URLParser(Parser):
-    def parse_staff_profile_link(self) -> dict:
+    def parse_teachers(self) -> dict[str, str]:
+        """Возвращает словарь c такой структурой:
+        ключ - ФИО учителя,
+        значение - URL-адрес его профиля.
         """
-        Возвращает словарь с именем учителя как ключ и ссылки на его профиль как значение
-        """
-        staff = {}
+        teachers: dict[str, str] = {}
 
-        response = requests.get(LINK_STAFF)
+        response = requests.get(TEACHER_URL)
 
         bs = BeautifulSoup(response.text, "html.parser")
 
-        all_text = bs.find_all("ul", class_ = 'staff_list')
+        all_text = bs.find_all("ul", class_="staff_list")
 
         for data in all_text:
             for li in data.find_all("li"):
-                staff_name = li.get_text().strip()
-                link = li.find("a").get("href")
-                staff[staff_name] = link
+                teacher_name = li.get_text().strip()
+                url = li.find("a").get("href")
+                teachers[teacher_name] = url
 
-        return staff
+        return teachers
