6809714bd391193543ce143818d8ec0696940668
MK1231223 <yaratest2143221314@gmail.com>
2025-02-28 23:11:18 +0300

    A kak kakat'?

--- src/telegram_assistant/parser/url_parser.py
+++ src/telegram_assistant/parser/url_parser.py
@@ -1,5 +1,26 @@
 from telegram_assistant.parser.parser import Parser
+from bs4 import BeautifulSoup, Comment, Script
+import requests
 
+LINK_STAFF = "http://www.mmcs.sfedu.ru/faculty/staff"
 
 class URLParser(Parser):
-    def parse(self, *args, **kwargs) -> dict[str, str]: ...
+    def parse_staff_profile_link(self) -> dict:
+        """
+        Возвращает словарь с именем учителя как ключ и ссылки на его профиль как значение
+        """
+        staff = {}
+
+        response = requests.get(LINK_STAFF)
+
+        bs = BeautifulSoup(response.text, "html.parser")
+
+        all_text = bs.find_all("ul", class_ = 'staff_list')
+
+        for data in all_text:
+            for li in data.find_all("li"):
+                staff_name = li.get_text().strip()
+                link = li.find("a").get("href")
+                staff[staff_name] = link
+
+        return staff
