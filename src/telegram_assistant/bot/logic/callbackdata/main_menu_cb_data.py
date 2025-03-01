4fb5d20a71d5df6b4ed25a97a5c3f4f6911c662a
RigalHD <profoundromeo@gmail.com>
2025-03-01 17:09:15 +0300

    -

--- src/telegram_assistant/bot/logic/callbackdata/main_menu_cb_data.py
+++ src/telegram_assistant/bot/logic/callbackdata/main_menu_cb_data.py
@@ -3,3 +3,9 @@ from aiogram.filters.callback_data import CallbackData
 
 class MainMenuCBData(CallbackData, prefix="main_menu"):
     action: str
+
+
+class QuestionCBData(CallbackData, prefix="question"):
+    action: str
+    question: str
+     
