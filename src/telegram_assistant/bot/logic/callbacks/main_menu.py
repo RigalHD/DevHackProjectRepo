e5b644c7124d31726799eeb7c7e7abf3e876f9af
RigalHD <profoundromeo@gmail.com>
2025-03-01 17:18:01 +0300

    still adding

--- src/telegram_assistant/bot/logic/callbacks/main_menu.py
+++ src/telegram_assistant/bot/logic/callbacks/main_menu.py
@@ -2,6 +2,7 @@ from aiogram import F, Router
 from aiogram.types import CallbackQuery
 
 from telegram_assistant.bot.logic.callbackdata import MainMenuCBData
+from telegram_assistant.bot.logic.callbackdata.main_menu_cb_data import QuestionCBData
 from telegram_assistant.bot.logic.keyboards.inline import main_menu_kb
 
 router = Router()
@@ -10,6 +11,15 @@ router = Router()
 @router.callback_query(MainMenuCBData.filter(F.action == "BackToMainMenu"))
 async def back_to_main_menu_handler(query: CallbackQuery) -> None:
     message_text = f"<b>Приветствую, {query.from_user.first_name}</b>\n\n"
+
+    await query.message.edit_text(
+        text=message_text,
+        reply_markup=main_menu_kb(),
+    )
+    
+@router.callback_query(QuestionCBData.filter(F.action == "AskQuestion"))
+async def ask_question_handler(query: CallbackQuery) -> None:
+    message_text = f""
 
     await query.message.edit_text(
         text=message_text,
