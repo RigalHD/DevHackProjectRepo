11c833da7b45843a4c3c738609ee1727b507ebd0
RigalHD <profoundromeo@gmail.com>
2025-03-01 17:18:01 +0300

    still adding

--- src/telegram_assistant/bot/logic/keyboards/inline/main_menu.py
+++ src/telegram_assistant/bot/logic/keyboards/inline/main_menu.py
@@ -2,14 +2,18 @@ from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 from telegram_assistant.bot.logic.callbackdata import MainMenuCBData
+from telegram_assistant.bot.logic.callbackdata.main_menu_cb_data import QuestionCBData
 
 
 def main_menu_kb() -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
     builder.row(
         InlineKeyboardButton(
-            text="Узнать o ЮФУ", # Расскажи о ЮФУ
-            callback_data=MainMenuCBData(action="BackToMainMenu").pack(),
+            text="Узнать o ЮФУ",
+            callback_data=QuestionCBData(
+                action="AskQuestion",
+                question="Расскажи о ЮФУ"
+                ).pack(),
         ),
         InlineKeyboardButton(
             text="Кафедры", # Расскажи о кафедрах ЮФУ
@@ -23,15 +27,21 @@ def main_menu_kb() -> InlineKeyboardMarkup:
     builder.row(
         InlineKeyboardButton(
             text="Поступление", # Расскажи о поступлении в ЮФУ
-            callback_data=MainMenuCBData(action="BackToMainMenu").pack(),
+            callback_data=QuestionCBData(
+                action="AskQuestion",
+                question="Расскажи о поступлении в ЮФУ",
+                ).pack(),
         ),
         InlineKeyboardButton(
             text="Расписание", # дадим ссылку
             callback_data=MainMenuCBData(action="BackToMainMenu").pack(),
         ),
         InlineKeyboardButton(
-            text="Мероприятия", # Расскажи о последних мероприятих ЮФУ, основываясь на постах
-            callback_data=MainMenuCBData(action="BackToMainMenu").pack(),
+            text="Мероприятия", # Расскажи о последних мероприятих ЮФУ + опционально(, основываясь на постах)
+            callback_data=QuestionCBData(
+                action="AskQuestion",
+                question="Расскажи о последних мероприятих ЮФУ",
+                ).pack(),
         ),
     )
                 
