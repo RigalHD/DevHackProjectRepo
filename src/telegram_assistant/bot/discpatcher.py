837d45dbefaa79e3493f3f8846694521a43fdb4b
RigalHD <profoundromeo@gmail.com>
2025-03-01 13:16:46 +0300

    removed PostgreSQL requirement

--- src/telegram_assistant/bot/discpatcher.py
+++ src/telegram_assistant/bot/discpatcher.py
@@ -3,22 +3,10 @@ from aiogram.fsm.storage.base import BaseStorage
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from telegram_assistant.bot.logic import logic_routers
-from telegram_assistant.bot.middlewares import routers as middlewares_routers
-from telegram_assistant.bot.middlewares.db_middleware import DatabaseMiddleware
-from telegram_assistant.bot.middlewares.perms_check_middleware import PermissionsCheckMiddleware
-from telegram_assistant.bot.middlewares.register_check_middleware import RegistrationCheckMiddleware
 
 storage: BaseStorage = MemoryStorage()
 dp = Dispatcher(storage=storage)
 
 dp.include_routers(
     *logic_routers,
-    *middlewares_routers,
 )
-
-dp.message.middleware(DatabaseMiddleware())
-dp.callback_query.middleware(DatabaseMiddleware())
-dp.message.middleware(RegistrationCheckMiddleware())
-dp.callback_query.middleware(RegistrationCheckMiddleware())
-dp.message.middleware(PermissionsCheckMiddleware())
-dp.callback_query.middleware(PermissionsCheckMiddleware())
--- src/telegram_assistant/bot/logic/callbackdata/admin_panel_cb_data.py
+++
@@ -1,5 +0,0 @@
-from aiogram.filters.callback_data import CallbackData
-
-
-class AdminPanelCBData(CallbackData, prefix="admin_panel"):
-    action: str
